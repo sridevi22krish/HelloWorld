8784bd4401c62b311d6f36073460fc744d961715
Training_b6b.01.03 <Training_b6b.01.03@accenture.com>
2018-10-05 12:41:41 +0530

    Add title Modified BG color

--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -3,6 +3,6 @@
 <title>HelloWorld</title>
 </head>
 <body BGCOLOR="LIGHTGREEN">
-<h2>Hello World! Wlcome to ACN</h2>
+<h2>Hello World! Welcome to ACN</h2>
 </body>
 </html>
--- src/test/java/org/ravi/helloworld/HelloTest.java
+++ src/test/java/org/ravi/helloworld/HelloTest.java
@@ -21,24 +21,24 @@ public class HelloTest {
 	public void beforeTest() {
 		/*	System.setProperty("webdriver.gecko.driver", "resources\\geckodriver32.exe");
 		driver = new FirefoxDriver();*/
-		/*System.setProperty("webdriver.chrome.driver","resources\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver","resources\\chromedriver.exe");
 		driver = new ChromeDriver();
-		driver.get("http://localhost:8080/Helloworld.war/");
-		driver.manage().timeouts().implicitlyWait(1, TimeUnit.MINUTES);*/
+		driver.get("http://localhost:8080/HelloWorld.war/");
+		driver.manage().timeouts().implicitlyWait(1, TimeUnit.MINUTES);
 	}
 
 	@Test
 	public void f() {
 
-		/*text = driver.findElement(By.xpath("html/body/h2"));
-		Assert.assertEquals(driver.getTitle(), "");
-		System.out.println("Test Passed");*/
+		//text = driver.findElement(By.xpath("html/body/h2"));
+		Assert.assertEquals(driver.getTitle(), "HelloWorld");
+		System.out.println("Test Passed");
 
 	}
 
 	@AfterTest
 	public void afterTest() {
-		//driver.close();
+		driver.close();
 	}
 
 
